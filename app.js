3427814c58a093638f63fbaeb2c34fe54d0115f7
Genar Trias Ortiz <genar@acs.li>
2016-12-21 04:01:33 +0100

    executing commands from config

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var nunjucks           = require('nunjucks');
-var path               = require('path');
-var fs                 = require('fs');
-var mkdirp             = require('mkdirp');
-var consul             = require('consul')();
-var config             = require('config');
-
-var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('.'));
+var nunjucks    = require('nunjucks');
+var path        = require('path');
+var fs          = require('fs');
+var mkdirp      = require('mkdirp');
+var consul      = require('consul')();
+var config      = require('config');
+var env         = new nunjucks.Environment(new nunjucks.FileSystemLoader('.'));
+var exec = require('child_process').exec;
 
 env.addFilter('split', function(str, seperator) {
     return str.split(seperator);
 });
 
 consul.agent.self(function(err, result) {
+    if (err) return console.log(err);
     var nodeName = result.Config.NodeName;
     startWatcher(nodeName);
 });
@@ -19,15 +20,21 @@ consul.agent.self(function(err, result) {
 function startWatcher(node) {
     var watch = consul.watch({ method: consul.catalog.node.services, options: {'node': node}});
 
-
     watch.on('change', function(data, res) {
         console.log(data.Services);
         config.get("templates").forEach(function (element) {
             var result = env.render(element.source, { data: data });
-            var templateDir = path.join(__dirname, element.path);
+            var templateDir = path.join(element.path);
             var filename = element.filename;
             mkdirp.sync(templateDir);
             fs.writeFileSync(path.join(templateDir, filename), result);
+
+            exec(element.command, function(error, stdout, stderr) {
+                if (error) return console.log(error);
+                console.log(stderr);
+                console.log(stdout);
+                  // command output is in stdout
+            });
 
             console.log(result);
         });
